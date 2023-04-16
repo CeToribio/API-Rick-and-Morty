b5878e6a2786d4dedd63bf64b693e663c7cb5251
CeToribio <anctoribio@gmail.com>
2023-04-16 12:45:45 -0500

    route and get data of one character

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,7 +1,26 @@
+import { useParams } from "react-router-dom";
+import { oneCharacter } from "../functions/function";
+import { useEffect, useState } from "react";
+
 function Character(){
+    const [character, setCharacter] = useState(null)
+
+    const {id} =useParams();
+    
+    useEffect(()=> {
+        oneCharacter(id,setCharacter)
+    })
     return(
         <>
-        <p>personajes</p>
+        {
+            character !=null ? 
+            <div>
+                <p>{character.name}</p>
+                <img src={character.image} alt="" />
+            </div>
+            : <p>Loading...</p> 
+            
+        }
         </>
     )
 
